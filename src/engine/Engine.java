9c695088e0aabdc5c612c7be6c0389ed35e5fd2e
Kevin Gregull <KevinGregull@GMail.com>
2012-09-14 00:07:14 +0200

    Window Name

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -6,11 +6,43 @@ import org.lwjgl.opengl.DisplayMode;
 
 public class Engine
 {
-	public Engine()
+	private static int width;
+	private static int height;
+	private static String title;
+	
+	public static int getWidth()
+	{
+		return Engine.width;
+	}
+	
+	public static int getHeight()
+	{
+		return Engine.height;
+	}
+	
+	public void setTitle(String title)
+	{
+		Engine.title=title;
+	}
+	
+	public static String getTitle()
+	{
+		return Engine.title;
+	}	
+	
+	public Engine(int width,int height)
+	{
+		// Define static Windowsize
+		Engine.width=width;
+		Engine.height=height;
+	}
+	
+	public void createWindow()
 	{
 		try
 		{
-			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setDisplayMode(new DisplayMode(Engine.getWidth(),Engine.getHeight()));
+			Display.setTitle(Engine.getTitle());
 			Display.create();
 		}
 		catch (LWJGLException ex)
--- src/glfx/GLFX.java
+++ src/glfx/GLFX.java
@@ -10,7 +10,9 @@ public class GLFX
 	
 	public static void main(String[] args)
 	{
-		Engine engine=new Engine();
+		Engine engine=new Engine(800,600);
+		engine.setTitle("OpenGL FrameworkX");
+		engine.createWindow();
 		engine.enterMainLoop();
 	}
 }
