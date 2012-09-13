a48aebc0711fb085779018b2d3246f366b1a7c23
Kevin Gregull <KevinGregull@GMail.com>
2012-09-13 23:29:37 +0200

    Jap

--- src/glfx/GLFX.java
+++ src/glfx/GLFX.java
@@ -8,11 +8,7 @@ public class GLFX
 	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Test");
-		
-		// Testout
-		System.out.println("Test");
+		System.out.println("TestIt");
 		return;
 
 	}
