15a419e4d22e68f103aa4c9f2e8c977af8e3e6e5
Kevin Gregull <KevinGregull@GMail.com>
2012-09-14 01:12:23 +0200

    Using SLICK to load Files to Texture

--- src/opengl/FXTexture.java
+++ src/opengl/FXTexture.java
@@ -1,6 +1,86 @@
 package opengl;
 
+import java.io.IOException;
+
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
 public class FXTexture
 {
-
+	private Texture texture;
+	
+	public void loadFromFile(String filename)
+	{
+		// Detect FILE Format
+		String getExtension=filename.toLowerCase();
+		String extension="NONE";
+		if (getExtension.contains(".jpg") || getExtension.contains(".jpeg"))
+		{
+			extension="JPG";
+		}
+		else if (getExtension.contains(".png"))
+		{
+			extension="PNG";
+		}
+		else
+		{
+			System.out.println("Invalid File Format for File '"+filename+"'");
+		}
+		
+		// Load the actual Texture
+		try
+		{
+			texture=TextureLoader.getTexture(extension,ResourceLoader.getResourceAsStream(filename));
+		}
+		catch(IOException e)
+		{
+			System.out.println("File '"+filename+"' could not be found!");
+		}
+	}
+	
+	public FXTexture(String filename)
+	{
+		loadFromFile(filename);
+	}
+	
+	public FXTexture()
+	{
+	}
+	
+	public int getWidth()
+	{
+		return texture.getImageWidth();
+	}
+	
+	public int getHeight()
+	{
+		return texture.getImageHeight();
+	}
+	
+	public int getTextureWidth()
+	{
+		return texture.getTextureWidth();
+	}
+	
+	public int getTextureHeight()
+	{
+		return texture.getTextureHeight();
+	}
+	
+	public void remove()
+	{
+		texture.release();
+	}
+	
+	public byte[] getData()
+	{
+		return texture.getTextureData();
+	}
+	
+	public int getTextureID()
+	{
+		return texture.getTextureID();
+	}
+	
 }
