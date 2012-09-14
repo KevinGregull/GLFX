1d6e18f1d23c035780ad2ed285549ea2d97c67ee
Kevin Gregull <KevinGregull@GMail.com>
2012-09-14 17:36:28 +0200

    First Shader modifications

--- src/opengl/FXShader.java
+++ src/opengl/FXShader.java
@@ -1,6 +1,20 @@
 package opengl;
 
+import static org.lwjgl.opengl.ARBFragmentShader.*;
+import static org.lwjgl.opengl.ARBShaderObjects.*;
+import static org.lwjgl.opengl.ARBVertexShader.*;
+
 public class FXShader
 {
-
+	private int shaderID;
+	private int vertID;
+	private int fragID;
+	
+	public FXShader(String vertfile,String fragfile)
+	{
+		glCreateProgramObjectARB();
+		this.vertID=glCreateShaderObjectARB(GL_VERTEX_SHADER_ARB);
+		
+		
+	}
 }
