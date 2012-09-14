23d80c925bbb1a652555b11166f693be4b3431cc
Kevin Gregull <KevinGregull@GMail.com>
2012-09-14 18:21:50 +0200

    Shader

--- src/opengl/FXShader.java
+++ src/opengl/FXShader.java
@@ -4,17 +4,38 @@ import static org.lwjgl.opengl.ARBFragmentShader.*;
 import static org.lwjgl.opengl.ARBShaderObjects.*;
 import static org.lwjgl.opengl.ARBVertexShader.*;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+
 public class FXShader
 {
 	private int shaderID;
 	private int vertID;
 	private int fragID;
 	
-	public FXShader(String vertfile,String fragfile)
+	public FXShader(String vertfilename,String fragfilename)
 	{
+		File vertfile=new File(vertfilename);
+		
+		
+		// Create Shader
 		glCreateProgramObjectARB();
+		
+		// Create VertexShader
 		this.vertID=glCreateShaderObjectARB(GL_VERTEX_SHADER_ARB);
+		glShaderSourceARB(this.vertID,"");
+		glCompileShaderARB(this.vertID);
 		
+		// Create FragmentShader
+		this.fragID=glCreateShaderObjectARB(GL_FRAGMENT_SHADER_ARB);
+		glShaderSourceARB(this.fragID,"");
+		glCompileShaderARB(this.fragID);
 		
+		// Attack Shaders to main Shader
+        glAttachObjectARB(this.shaderID,this.vertID);
+        glAttachObjectARB(this.shaderID,this.fragID);
+        glLinkProgramARB(this.shaderID);
+        glValidateProgramARB(this.shaderID);
 	}
 }
